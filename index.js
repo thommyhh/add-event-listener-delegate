e0ffe77d950ea91b9b4ff14c96027ac8ec876db7
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2018-08-28 19:02:18 +0200

    [TASK] Improve method signature
    * switch `eventType` and `selector` to better match `jQuery.on()`s signature
    * remove useless `stopAtFirstMatch` argument

--- index.js
+++ index.js
@@ -2,21 +2,18 @@
  * Add an event listener to the given element, that triggers on child elements with the given selector
  *
  * @param element {Element}
- * @param selector {string}
  * @param eventType {string}
+ * @param selector {string}
  * @param callback {function(ev: Event, element: Element)}
- * @param stopAtFirstMatch {boolean}
  */
-export default function(element, selector, eventType, callback, stopAtFirstMatch = true) {
+export default function(element, eventType, selector, callback) {
 	element.addEventListener(eventType, ev => {
 		/** @type {Element} */
 		let node = ev.target
 		while (node !== element) {
 			if (node.matches(selector)) {
 				callback(ev, node)
-				if (stopAtFirstMatch) {
-					break
-				}
+				break
 			}
 			node = node.parentElement
 		}
